dfcbb25ad8d29552d1177f59ff844550acda000a
BharathSanjeeviT <bharathsanjeevi.t@gmail.com>
2023-04-20 15:09:28 +0530

    API using Express

---
+++ index.js
@@ -0,0 +1,69 @@
+const express  = require("express")
+const app = express()
+
+const bodyparser = require("body-parser");
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({extended : false}))
+
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient()
+
+app.get('/',(req,res)=> {
+    const dbFunc = async () => {
+        const data = await prisma.User.findMany()
+        res.send(data)
+    }
+    dbFunc()
+})
+
+app.get('/redirect',(req,res)=>{
+    res.redirect("http://"+"krunker"+".io")
+})
+
+app.post('/create_user',(req,res)=> {
+     
+    const postData = async () => {
+        const data = await prisma.User.create({
+            data : {
+                id : req.body.id,
+                name : req.body.name
+            },
+        })
+        res.send(data)
+    }
+    postData()
+})
+
+app.post('/trans',(req,res)=>{
+
+    const create_trans = async () => {
+        const data = await prisma.Transaction.create({
+            data :  {
+                userId : req.body.uid,
+                petId : 1,
+                transaction : req.body.amount,
+            },
+        }) 
+        res.send(data);
+    } 
+    create_trans()
+})
+
+app.post('/getTrans',(req,res)=>{
+    
+    const get_trans = async () => {
+        const data = await prisma.user.findUnique({
+            where : {
+                id : req.body.id,
+            },
+            include : {
+                transUser : true,
+            },
+        })
+        res.send(data.transUser)
+    }
+    get_trans();
+})
+
+app.listen(process.env.PORT || 3000)
+
